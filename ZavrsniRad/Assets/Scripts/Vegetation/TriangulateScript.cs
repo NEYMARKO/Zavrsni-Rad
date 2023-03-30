358755ecb5a1a8aa60a3ec63641faa19d373df69
NEYMARKO <marko.prosenjak5@gmail.com>
2023-03-30 11:27:48 +0200

    Changed TriangulateScript a bit, added cylinder to the scene

--- ZavrsniRad/Assets/Scripts/Vegetation/TriangulateScript.cs
+++ ZavrsniRad/Assets/Scripts/Vegetation/TriangulateScript.cs
@@ -7,9 +7,15 @@ public class TriangulateScript : MonoBehaviour
 {
 
     public Terrain terrain;
+    private int trianglesInTerrain;
     private float[] steepnessArray;
-    
+    private float[] triangleCentroidPositionArray;
     // Start is called before the first frame update
+    private void Awake()
+    {
+        trianglesInTerrain = (int)(terrain.terrainData.size.x - 1) * (int)(terrain.terrainData.size.z - 1) * 2;
+        triangleCentroidPositionArray = new float[trianglesInTerrain];
+    }
     void Start()
     {
         steepnessArray = Triangulate();
@@ -18,7 +24,7 @@ public class TriangulateScript : MonoBehaviour
     
     private float[] Triangulate()
     {
-        float[] steepness = new float[(int)(terrain.terrainData.size.x - 1) * (int)(terrain.terrainData.size.z - 1) * 2];   //no. of tris = (no. of points - 1)^2 * 2- dobiveno na papiru
+        float[] steepness = new float[trianglesInTerrain];   //no. of tris = (no. of points - 1)^2 * 2- dobiveno na papiru
         for (int j = 0; j < terrain.terrainData.size.z; j++)
         {
             for (int i = 0; i < terrain.terrainData.size.x - 1; i++)
@@ -30,25 +36,25 @@ public class TriangulateScript : MonoBehaviour
                 float widthCentroidTris2 = (3 * i + 2) / (3f * terrain.terrainData.size.x);
                 float lengthCentroidTris2 = (3 * j + 2) / (3f * terrain.terrainData.size.z);
                 steepness[j * i * 2 + 1] = terrain.terrainData.GetSteepness(widthCentroidTris2, lengthCentroidTris2);
+
+                setCentroidPositionArray(i, j);
             } 
         }
         return steepness;
     }
-    /*private void Triangulate()
+
+    private void setCentroidPositionArray(int i, int j)
     {
-        int x = 0;
-        int width = (int)terrain.terrainData.size.x; 
-        for (int z = 0; z < terrain.terrainData.size.x * terrain.terrainData.size.z; z += 3) 
-        {
-            if (z % width == 0)
-            {
-                x++;
-            }
-            for (int i = 0; i < 3; i++)
-            {
-                Vector3 position = new Vector3(x, 0, z);
-                position.y = terrain.terrainData.GetInterpolatedHeight(x / terrain.terrainData.size.x, z / terrain.terrainData.size.z);
-            }
-        }
-    }*/
+        triangleCentroidPositionArray[j * i * 2] = (3 * i + 1) / (3f * terrain.terrainData.size.x);
+        triangleCentroidPositionArray[j * i * 2 + 1] = (3 * i + 2) / (3f * terrain.terrainData.size.x);
+    }
+    public float[] getSteepnessArray()
+    {
+        return steepnessArray;
+    }
+
+    public float[] getCentroidPositionArray()
+    {
+        return triangleCentroidPositionArray;
+    }
 }
